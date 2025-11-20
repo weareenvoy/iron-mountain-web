9a77e601a2aa6d00e1b52ee4a3433a5df60ca77c
Steven Grimaldo <steven@weareenvoy.com>
2025-11-19 22:28:18 -0800

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,19 @@ import { APPS } from '@/lib/internal/contants';
 
 const Home = ({}: PageProps<'/'>) => {
   return (
-    <div className="flex min-h-svh items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Apps:</h1>
-        <div className="flex flex-col gap-2">
+    <div className="flex min-h-screen items-center justify-center bg-background font-sans">
+      <main className="flex w-full max-w-3xl flex-col items-center gap-y-8 px-16 py-32 sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">Apps:</h1>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           {APPS.map((app, index) => (
-            <Button asChild key={index} size="xl">
+            <Button asChild className="h-12 md:w-[158px]" key={index} size="xl">
               <Link href={app.route}>{app.title}</Link>
             </Button>
           ))}
         </div>
-      </div>
+      </main>
     </div>
   );
 };
