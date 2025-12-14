08c01db29851c33afb9fbbf1ce6e39d2cdea290e
Marcus Graham <marcus@weareenvoy.com>
2025-12-13 18:38:46 -0800

    cleanup

--- src/app/(displays)/(kiosks)/_components/kiosk-templates/solution/thirdScreen/thirdScreenTemplate.tsx
+++ src/app/(displays)/(kiosks)/_components/kiosk-templates/solution/thirdScreen/thirdScreenTemplate.tsx
@@ -201,7 +201,7 @@ const SolutionThirdScreenTemplate = ({
         role="button"
         tabIndex={0}
       >
-      <ArrowUp aria-hidden="true" className="h-full w-full text-[#6DCFF6]" focusable="false" strokeWidth={1.5} />
+        <ArrowUp aria-hidden="true" className="h-full w-full text-[#6DCFF6]" focusable="false" strokeWidth={1.5} />
       </div>
       <div
         aria-label="Next"
@@ -216,7 +216,7 @@ const SolutionThirdScreenTemplate = ({
         role="button"
         tabIndex={0}
       >
-      <ArrowDown aria-hidden="true" className="h-full w-full text-[#6DCFF6]" focusable="false" strokeWidth={1.5} />
+        <ArrowDown aria-hidden="true" className="h-full w-full text-[#6DCFF6]" focusable="false" strokeWidth={1.5} />
       </div>
     </div>
   );
