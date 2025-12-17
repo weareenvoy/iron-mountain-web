1fb8040765daa77f8f816a4386826cf249df50eb
Daniel Aharodnikau <daharodnikau@weareenvoy.com>
2025-12-16 19:21:40 -0800

    fix: build fixes

--- src/app/(displays)/summit/slides/page.tsx
+++ src/app/(displays)/summit/slides/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import SummitSlideLayout from '@/app/(displays)/summit/_components/layouts/summit-slide-layout';
 import SummitSlidesScreen from '@/app/(displays)/summit/_components/slides/summit-slides-screen';
 
@@ -7,12 +8,16 @@ const SummitSlidesSplitPage = async () => {
       <div className="flex h-full w-full">
         <div className="flex h-full flex-1 items-center justify-center bg-background">
           <div className="flex h-full w-full items-center justify-center" style={{ aspectRatio: '16 / 9' }}>
-            <SummitSlidesScreen screen="primary" />
+            <Suspense fallback={null}>
+              <SummitSlidesScreen screen="primary" />
+            </Suspense>
           </div>
         </div>
         <div className="flex h-full flex-1 items-center justify-center bg-background">
           <div className="flex h-full w-full items-center justify-center" style={{ aspectRatio: '16 / 9' }}>
-            <SummitSlidesScreen screen="secondary" />
+            <Suspense fallback={null}>
+              <SummitSlidesScreen screen="secondary" />
+            </Suspense>
           </div>
         </div>
       </div>
