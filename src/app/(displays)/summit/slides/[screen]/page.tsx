d6b01eec7253e533a9bd9b467aa18cb6e2be9a24
Daniel Aharodnikau <daharodnikau@weareenvoy.com>
2025-12-16 19:15:24 -0800

    fix: build fixes

--- src/app/(displays)/summit/slides/[screen]/page.tsx
+++ src/app/(displays)/summit/slides/[screen]/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation';
 
+export const generateStaticParams = () => {
+  return [{ screen: 'primary' }, { screen: 'secondary' }];
+};
+
 const LegacySummitSlideScreenRedirect = () => {
   redirect('/summit/slides');
 };
