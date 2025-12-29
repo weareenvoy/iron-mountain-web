80136cbf9fa0d03ce08a6dcb03ab6466b2822aa7
Marcus Graham <marcus@weareenvoy.com>
2025-12-29 14:42:10 -0800

    Fix linter issues and restore kioskId prop in secondScreenTemplate

--- src/app/(displays)/(kiosks)/_components/kiosk-templates/solution/secondScreen/secondScreenTemplate.tsx
+++ src/app/(displays)/(kiosks)/_components/kiosk-templates/solution/secondScreen/secondScreenTemplate.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import AnimatedNumberedList from '@/app/(displays)/(kiosks)/_components/kiosk-templates/solution/secondScreen/AnimatedNumberedList';
-import { DEFAULT_KIOSK_ID, type KioskId } from '@/app/(displays)/(kiosks)/_types/kiosk-id';
+import { type KioskId } from '@/app/(displays)/(kiosks)/_types/kiosk-id';
 import BlueDiamondSecond from '@/components/ui/icons/Kiosks/Solutions/BlueDiamondSecond';
 import GreenDiamondSecond from '@/components/ui/icons/Kiosks/Solutions/GreenDiamondSecond';
 import OutlinedDiamond from '@/components/ui/icons/Kiosks/Solutions/OutlinedDiamond';
@@ -45,6 +45,7 @@ type SolutionSecondScreenStepsProps = {
 const SolutionSecondScreenTemplate = ({
   heroImageAlt,
   heroImageSrc,
+  kioskId,
   labelText,
   numberedListHeadline,
   onRegisterListHandlers,
@@ -81,22 +82,23 @@ const SolutionSecondScreenTemplate = ({
 
   return (
     <div
-      className="relative flex h-screen w-full flex-col overflow-visible bg-transparent"
+      className="group/kiosk relative flex h-screen w-full flex-col overflow-visible bg-transparent"
+      data-kiosk={kioskId}
       data-scroll-section="solution-second-group"
     >
       {/* Gradient backdrop */}
       <div className="absolute top-[-296px] left-0 h-[14575px] w-full rounded-t-[100px] bg-transparent" />
 
       {/* Decorative diamonds */}
-      <div className="pointer-events-none absolute top-[1080px] left-[-100px] z-[1] h-[1790px] w-[1790px]">
+      <div className="pointer-events-none absolute top-[1080px] left-[-100px] z-1 h-[1790px] w-[1790px]">
         <BlueDiamondSecond aria-hidden="true" className="h-full w-full" focusable="false" />
       </div>
-      <div className="pointer-events-none absolute top-[970px] left-[1270px] z-[1] h-[890px] w-[890px] group-data-[kiosk=kiosk-2]/kiosk:top-[1040px] group-data-[kiosk=kiosk-2]/kiosk:left-[1330px] group-data-[kiosk=kiosk-2]/kiosk:size-[800px] group-data-[kiosk=kiosk-3]/kiosk:top-[1050px] group-data-[kiosk=kiosk-3]/kiosk:left-[1330px] group-data-[kiosk=kiosk-3]/kiosk:size-[810px]">
+      <div className="pointer-events-none absolute top-[970px] left-[1270px] z-1 h-[890px] w-[890px] group-data-[kiosk=kiosk-2]/kiosk:top-[1040px] group-data-[kiosk=kiosk-2]/kiosk:left-[1330px] group-data-[kiosk=kiosk-2]/kiosk:size-[800px] group-data-[kiosk=kiosk-3]/kiosk:top-[1050px] group-data-[kiosk=kiosk-3]/kiosk:left-[1330px] group-data-[kiosk=kiosk-3]/kiosk:size-[810px]">
         <GreenDiamondSecond aria-hidden="true" className="h-full w-full" focusable="false" />
       </div>
 
       {/* Subheadline */}
-      <h2 className="absolute top-[230px] left-[120px] z-[1] w-[500px] text-[60px] leading-[1.4] font-normal tracking-[-3px] whitespace-pre-line text-[#ededed]">
+      <h2 className="absolute top-[230px] left-[120px] z-1 w-[500px] text-[60px] leading-[1.4] font-normal tracking-[-3px] whitespace-pre-line text-[#ededed]">
         {renderRegisteredMark(subheadline)}
       </h2>
 
@@ -111,7 +113,7 @@ const SolutionSecondScreenTemplate = ({
       </div>
 
       {/* Title */}
-      <p className="absolute top-[1600px] left-[240px] z-[1] text-[100px] leading-[1.3] font-normal tracking-[-5px] text-white group-data-[kiosk=kiosk-2]/kiosk:top-[1570px] group-data-[kiosk=kiosk-2]/kiosk:left-[250px]">
+      <p className="absolute top-[1600px] left-[240px] z-1 text-[100px] leading-[1.3] font-normal tracking-[-5px] text-white group-data-[kiosk=kiosk-2]/kiosk:top-[1570px] group-data-[kiosk=kiosk-2]/kiosk:left-[250px]">
         {renderRegisteredMark(numberedListHeadline)}
       </p>
 
@@ -123,12 +125,12 @@ const SolutionSecondScreenTemplate = ({
       />
 
       {/* Hero media diamond */}
-      <div className="absolute top-[2210px] left-[1500px] z-[2]">
-        <div className="relative top-[-140px] left-[-230px] h-[900px] w-[900px] rotate-[45deg] overflow-hidden rounded-[80px] group-data-[kiosk=kiosk-2]/kiosk:top-[-160px] group-data-[kiosk=kiosk-2]/kiosk:left-[-180px] group-data-[kiosk=kiosk-2]/kiosk:size-[810px] group-data-[kiosk=kiosk-3]/kiosk:top-[-130px] group-data-[kiosk=kiosk-3]/kiosk:left-[-160px] group-data-[kiosk=kiosk-3]/kiosk:size-[800px]">
+      <div className="absolute top-[2210px] left-[1500px] z-2">
+        <div className="relative top-[-140px] left-[-230px] h-[900px] w-[900px] rotate-45 overflow-hidden rounded-[80px] group-data-[kiosk=kiosk-2]/kiosk:top-[-160px] group-data-[kiosk=kiosk-2]/kiosk:left-[-180px] group-data-[kiosk=kiosk-2]/kiosk:size-[810px] group-data-[kiosk=kiosk-3]/kiosk:top-[-130px] group-data-[kiosk=kiosk-3]/kiosk:left-[-160px] group-data-[kiosk=kiosk-3]/kiosk:size-[800px]">
           {heroImageSrc && (
             <Image
               alt={heroImageAlt || ''}
-              className="-rotate-[45deg] object-cover"
+              className="-rotate-45 object-cover"
               fill
               sizes="800px"
               src={heroImageSrc}
